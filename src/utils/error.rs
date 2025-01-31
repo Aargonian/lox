124c9b673e7dd85d37f03bbab878d608d7c15abe
Aaron Gorodetzky <aaron@nytework.com>
2025-01-30 23:00:13 -0500

    Starting Over

--- src/utils/error.rs
+++
@@ -1,8 +0,0 @@
-#![allow(dead_code)]
-pub fn error(line: usize, message: &str) {
-    report(line, "", message);
-}
-
-pub fn report(line: usize, were: &str, message: &str) {
-    eprintln!("[Line {line}] Error {were}: {message}");
-}
